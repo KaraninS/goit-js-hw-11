52cbbcb6ff7899f1e36b584cd328a3a606e5eaa9
Karanin Serhii <36915523+KaraninS@users.noreply.github.com>
2022-06-05 11:46:06 +0300

    update files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+//=== Прокрутка страниц====
+// const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
+// ===============
